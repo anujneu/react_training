23576eb68ad3525f97da37d3aeb02ae0e0583902
anujneu <anujneupane68@gmail.com>
2025-12-26 13:01:15 +0545

    anime quotes

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,84 @@
-import { use, useState } from "react";
+import { useState } from "react";
 import "./App.css";
-import Input from "./input";
 
 function App() {
-  const [todolist, settodolist] = useState([]);
-  const [input, setInput] = useState("");
+  const [quotes, setQuotes] = useState([]);
+  const [charName, setCharName] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
 
-  const handleAdd = () => {
-    if (!input) return;
-    settodolist([...todolist, input]);
-    setInput("");
+  const fetchQuote = async () => {
+    if (!charName) return;
+
+    setLoading(true);
+    setError("");
+    setQuotes([]);
+    try {
+      const response = await fetch(
+        `https://yurippe.vercel.app/api/quotes?character=${charName}&random=1`
+      );
+      const result = await response.json();
+
+      if (response.status === 404) {
+        throw new Error("Character not found");
+      }
+      setQuotes(result);
+    } catch (e) {
+      setError(e.message);
+    } finally {
+      setLoading(false);
+    }
   };
-  console.log(settodolist);
 
-  const type = (event) => {
-    let value = event.target.value;
-    setInput(value);
+  const handleSubmit = (e) => {
+    e.preventDefault(); // stop page refresh
+    fetchQuote();
+  };
+
+  const handleKeyUp = (e) => {
+    if (e.key === "Enter") {
+      fetchQuote();
+    }
+  };
+
+  const handleInput = (e) => {
+    const value = e.target.value;
+    setCharName(value);
   };
 
   return (
     <>
-      <h1>{input}</h1>
-      <input
-        value={input}
-        type="text"
-        placeholder="Enter todo list"
-        onChange={type}
-      ></input>
-      <button onClick={handleAdd}>ADD</button>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={charName}
+          onChange={handleInput}
+          placeholder="Enter character name"
+        />
+
+        <button type="submit">Search</button>
+      </form>
+
+      {loading && <p>Loading...</p>}
+      {error && <p>{error}</p>}
+
       <ul>
-        {todolist.map((item, index) => (
-          <li key={index}>{item}</li>
+        {quotes.map((item, index) => (
+          <li key={index}>
+            <p>
+              <b>Show:</b> {item.show}
+            </p>
+            <p>
+              <b>Quote:</b> {item.quote}
+            </p>
+            <p>
+              <b>Character:</b> {item.character}
+            </p>
+          </li>
         ))}
       </ul>
     </>
   );
 }
-
-// if (showCounter) {
-//   hello;
-// } else {
-//   bye;
-// }
-
-// 3.push,pull,1.add,2.commit(git hub )
-//boot.dev
 
 export default App;
